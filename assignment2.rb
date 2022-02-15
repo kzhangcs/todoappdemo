0a7045abcf1f77b53e9cb409e71cafdb6dd1a27b
Kristine Zhang <kristinez@berkeley.edu>
2022-02-14 20:40:44 -0800

    pattern match subdomain

--- assignment2.rb
+++ assignment2.rb
@@ -1,5 +1,5 @@
 word_frequency = Hash.new(0)
-pattern = /([a-z]+.com)[.]*/i
+pattern = /[.]*([a-z]+.com)[.]*/i
 
 # line_arr = []
 File.open("assignment2.txt", "r:ISO-8859-1") do |file|
@@ -7,7 +7,7 @@ File.open("assignment2.txt", "r:ISO-8859-1") do |file|
     words = line.encode("utf-8", replace: nil).split
     words.each do |word|
       case word
-        when /[a-z]+.com/i then 
+        when pattern then 
         result = word.match(pattern)
         domain = result[1]
         word_frequency[domain] += 1
