40f5f01777f3f93f24f886ee4f697608340e3b76
Kristine Zhang <kristinez@berkeley.edu>
2022-02-14 21:11:35 -0800

    fix text file name, subdomain match, domain names with '-'

--- assignment2.rb
+++ assignment2.rb
@@ -1,15 +1,15 @@
 word_frequency = Hash.new(0)
-pattern = /[.]*@([a-z\d]+\.com)[.]*/i
+pattern = /[.]*@([a-z\d-]+\.)*([a-z\d-]+\.com)[.]*/i
 
 # line_arr = []
-File.open("assignment2.txt", "r:ISO-8859-1") do |file|
+File.open("assignment_two_text.txt", "r:ISO-8859-1") do |file|
   file.each_line { |line| 
     words = line.encode("utf-8", replace: nil).split
     words.each do |word|
       case word
         when pattern then 
         result = word.match(pattern)
-        domain = result[1]
+        domain = result[2]
         word_frequency[domain] += 1
       end
     end
