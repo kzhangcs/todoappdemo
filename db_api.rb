9b15917a0ce6337b583450c9c39d3ce708174808
Kristine Zhang <kristinez@berkeley.edu>
2022-02-22 02:03:16 -0800

    add other selects

--- db_api.rb
+++ db_api.rb
@@ -4,7 +4,7 @@ require 'yaml'
 
 class DbApi  
 
-  @@students =   YAML::load_file('university_db.yml')
+  @@students = YAML::load_file('university_db.yml')
 
   # def initializer()
   #   @@students =  YAML::load_file('university_db.yml')
@@ -15,11 +15,20 @@ class DbApi
   end
 
   def self.select_by_gender (gender)
-    students2 = DbApi.students
-    p students2[0]
-    selected = students2.select{|student| gender === student.gender}
+    students = DbApi.students
+    selected = students.select{|student| gender === student.gender}
   end
-  # def self.select_by_first_name (first_name) ... end 
-  # def self.select_by_last_name (last_name) ... end 
-  # def self.select_by_weight_more_than(pounds) ... end
+
+  def self.select_by_first_name (first_name)
+    students = DbApi.students
+    selected = students.select{|student| first_name =~ student.first_name} #todo: regex
+  end 
+  def self.select_by_last_name (last_name)
+    students = DbApi.students
+    selected = students.select{|student| last_name =~ student.last_name} #todo: regex  
+  end 
+  def self.select_by_weight_more_than(pounds)
+    students = DbApi.students
+    selected = students.select{|student| pounds < student.pounds}
+  end
 end
