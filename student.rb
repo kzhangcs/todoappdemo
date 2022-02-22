e46b5d74562a4eb15d6b01b081afde793762c035
Kristine Zhang <kristinez@berkeley.edu>
2022-02-22 02:07:25 -0800

    edit student display

--- student.rb
+++ student.rb
@@ -5,7 +5,7 @@ class Student
     yield self if block_given?
   end
 
-  def to_s
-    "#{student_id} #{first_name} #{last_name} #{city} #{state} #{email} #{gender} #{pounds} #{gpa} #{taking_courses}"
+  def to_s #todo: delete student_id
+    "Name: #{first_name} #{last_name} (#{gender} from #{city}, #{state}) weight: #{pounds} GPA: #{gpa} Courses: #{taking_courses}"
   end
 end
