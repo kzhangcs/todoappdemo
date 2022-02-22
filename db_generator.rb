bf701feed76b5589f473395b943d2fa31d07e091
Kristine Zhang <kristinez@berkeley.edu>
2022-02-21 20:23:16 -0800

    fix classes, try reading CSV

---
+++ db_generator.rb
@@ -0,0 +1,28 @@
+# CSV library for Ruby
+#
+# - reads a file containing csv data and processes line by line
+
+require 'csv'
+require_relative 'student.rb'
+require_relative 'course.rb'
+
+# - processes a CSV file that contains a header line. Automatically converts
+# fields that look like numbers.
+students = []
+total_cost = 0
+CSV.foreach("students.csv", headers: true, converters: :numeric) do |data|
+  student = Student.new
+  student.student_id = 0 #todo
+  student.first_name = data["GivenName"]
+  student.last_name = data["Surname"]
+  student.city = data["City"]
+  student.state = data["State"]
+  student.email = data["EmailAddress"]
+  student.gender = data["Gender"]
+  student.pounds = data["Pounds"]
+  student.gpa = 0 #todo
+  student.taking_courses = 0 #todo
+  students << student
+end
+
+puts students
