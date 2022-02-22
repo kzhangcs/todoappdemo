4a4300d8e40dd01719fd662c1de754f372556d49
Kristine Zhang <kristinez@berkeley.edu>
2022-02-21 19:44:02 -0800

    delete assignment2 files

---
+++ course.rb
@@ -0,0 +1,11 @@
+class Course
+  attr_accessor :course_name, :course_id
+
+  def initialize(course_name, course_id)
+    @course_name = course_name
+    @course_id = course_id
+  end
+
+  def to_s
+    "#{course_name} #{course_id}"
+  end
