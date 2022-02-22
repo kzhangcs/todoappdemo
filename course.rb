ee28f0b539f1b3ec69defee7b122c2935f6314ec
Kristine Zhang <kristinez@berkeley.edu>
2022-02-22 03:26:45 -0800

    test run, forgot to save courses

--- course.rb
+++ course.rb
@@ -1,11 +1,12 @@
 class Course
   attr_accessor :course_name, :course_id
 
-  def initialize(course_name, course_id)
-    @course_name = course_name
+  def initialize(course_id, course_name)
     @course_id = course_id
+    @course_name = course_name
   end
 
   def to_s
-    "#{course_name} #{course_id}"
+    "#{course_id} - #{course_name}"
   end
+end
