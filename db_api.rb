b28120f051d85c15062103aa562e7af8b1f68e4b
Kristine Zhang <kristinez@berkeley.edu>
2022-02-28 07:27:22 -0800

    clean up

--- db_api.rb
+++ db_api.rb
@@ -6,20 +6,6 @@ class DbApi
 
   @@students = []
 
-  # def initializer()
-  #   puts "here"
-  #   @students =  YAML::load_file('university_db.yml')
-  # end
-
-  # def self.students #class method
-  #   @@students
-  # end
-
-  # def self.method_missing(method, *args)
-  #     @@students = YAML::load_file('university_db.yml')
-  #     puts students[0]
-  # end
-
   def self.method_missing (method, *args)
     if @@students.empty?
       @@students = YAML::load_file('university_db.yml')
@@ -27,28 +13,10 @@ class DbApi
     students_array = @@students
     
     compare = args[0]
-    searching = args[1] 
+    criteria = args[1] 
     result = method.match(/select_students_where_(.*)/i)
     attribute = result[1]
     
-    selected = students_array.select{|student| (student.send attribute).send(compare, searching)}
+    selected = students_array.select{|student| (student.send attribute).send(compare, criteria)}
   end
-  
-  # def self.select_by_gender (gender)
-  #   students = YAML::load_file('university_db.yml')
-  #   selected = students.select{|student| gender === student.gender}
-  # end
-
-#   def self.select_by_first_name (first_name)
-#     students = DbApi.students
-#     selected = students.select{|student| first_name =~ student.first_name}
-#   end 
-#   def self.select_by_last_name (last_name)
-#     students = DbApi.students
-#     selected = students.select{|student| last_name =~ student.last_name} 
-#   end 
-#   def self.select_by_weight_more_than(pounds)
-#     students = DbApi.students
-#     selected = students.select{|student| pounds < student.pounds}
-#   end
 end
