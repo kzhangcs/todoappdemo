f6afda0fc93a27ec15f888328e48d0668ae79a60
Kristine Zhang <kristinez@berkeley.edu>
2022-03-08 13:17:25 -0800

    trying to add parameter to URL

--- stock_quotes/app/models/stock_quote.rb
+++ stock_quotes/app/models/stock_quote.rb
@@ -9,10 +9,12 @@ class StockQuote
   #   get('/getall_reps_bystate.php', query: { state: state})['results']
   # end
 
-   API_URL = 'https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_9de4cd0891cf444ab35b6caae35fde3a'
+   # API_URL = 'https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_9de4cd0891cf444ab35b6caae35fde3a'
 
   def unique_url
-    response = HTTParty.get(API_URL)
+  	base_uri = 'https://cloud.iexapis.com/stable/stock/'
+ 	full_url = base_uri + 'aapl' + '/quote?token=pk_9de4cd0891cf444ab35b6caae35fde3a'
+ 	response = HTTParty.get(full_url)
     # TODO more error checking (500 error, etc)
     JSON.parse(response.body)
   end
