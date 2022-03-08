461c735d1bc077d7bcc05ca427d9e5081af78b11
Kristine Zhang <kristinez@berkeley.edu>
2022-03-08 12:59:15 -0800

    try extracting companyName using API get

--- stock_quotes/app/controllers/stock_quotes_controller.rb
+++ stock_quotes/app/controllers/stock_quotes_controller.rb
@@ -1,7 +1,18 @@
 class StockQuotesController < ApplicationController
   def index
-    @state = params[:state] || 'md'
-    @reps = StockQuote.for(@state.upcase!).
-      sort_by {|rep| rep['name']}
+    # @state = params[:state] || 'md'
+    # @reps = StockQuote.for(@state.upcase!).
+    #   sort_by {|rep| rep['name']}
+
+     # @result = StockQuote.for('MD')
+     # @companyName = 'Apple'
+
+    api = StockQuote.new()
+    result = api.unique_url
+    url = result
+    @companyName = url
+    # @user = # Code to retrieve User
+    # @user.update_attribute :url, url
+    # etc
   end
 end
--- stock_quotes/app/models/stock_quote.rb
+++ stock_quotes/app/models/stock_quote.rb
@@ -1,11 +1,20 @@
 class StockQuote
   include HTTParty
 
-  base_uri 'http://whoismyrepresentative.com'
-  default_params output: 'json'
-  format :json
+  # base_uri 'http://whoismyrepresentative.com'
+  # default_params output: 'json'
+  # format :json
 
-  def self.for state
-    get('/getall_reps_bystate.php', query: { state: state})['results']
+  # def self.for state
+  #   get('/getall_reps_bystate.php', query: { state: state})['results']
+  # end
+
+   API_URL = 'https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_9de4cd0891cf444ab35b6caae35fde3a'
+
+  def unique_url
+    response = HTTParty.get(API_URL)
+    # TODO more error checking (500 error, etc)
+    json = JSON.parse(response.body)
+    json['companyName']
   end
 end
