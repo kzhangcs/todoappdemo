93858b76acb78729f0153f7f6611b7431c2c2570
Kristine Zhang <kristinez@berkeley.edu>
2022-03-08 20:43:16 -0800

    clean up

--- stock_quotes/app/controllers/stock_quotes_controller.rb
+++ stock_quotes/app/controllers/stock_quotes_controller.rb
@@ -1,24 +1,8 @@
 class StockQuotesController < ApplicationController
   def index
-    # @state = params[:state] || 'md'
-    # @reps = StockQuote.for(@state.upcase!).
-    #   sort_by {|rep| rep['name']}
-
-     # @result = StockQuote.for('MD')
-     # @companyName = 'Apple'
-
     api = StockQuote.new()
     inputs = params[:symbol].split(',') || ['aapl','fb']
     @result = api.query(inputs)
-   	# @params = params
-   	# @inputs = inputs
    	@companies = params[:symbol] || 'aapl,fb'
-    # @first = @result[0]
-    # @symbol = @first[:symbol] #was: @symbol = @first['symbol']
-    # @companyName = @first[:companyName]
-    # @change = params[:change] || '0.61'
-    # @user = # Code to retrieve User
-    # @user.update_attribute :url, url
-    # etc
   end
 end
--- stock_quotes/app/models/stock_quote.rb
+++ stock_quotes/app/models/stock_quote.rb
@@ -1,41 +1,18 @@
 class StockQuote
   include HTTParty
 
-  # base_uri 'http://whoismyrepresentative.com'
-  # default_params output: 'json'
-  # format :json
-
-  # def self.for state
-  #   get('/getall_reps_bystate.php', query: { state: state})['results']
-  # end
-
-   # API_URL = 'https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_9de4cd0891cf444ab35b6caae35fde3a'
-   # example input: FB, AAPL, MSFT, AMZN
-  def query inputs #todo: method name
+  def query inputs
   	array = []
-  	# inputs = ["fb", "amzn", "appl"]
+
   	for input in inputs
 	  	base_uri = 'https://cloud.iexapis.com/stable/stock/'
 	 	full_url = base_uri + input + '/quote?token=pk_9de4cd0891cf444ab35b6caae35fde3a'
 	 	response = HTTParty.get(full_url)
-	 	parsed = JSON.parse(response.body) #todo: crashes when symbol not found
+	 	parsed = JSON.parse(response.body)
 	 	array << parsed
-	 end
-	 array
-    # TODO more error checking (500 error, etc)
-    # JSON.parse(response.body)
- #    [
-	#   	{
-	#     "change": 0.61,
-	#     "companyName": "Apple",
-	#     "symbol": "AAPL"
-	# 	},
+	end
 
-	# 	{
-	#     "change": 2.79,
-	#     "companyName": "Meta",
-	#     "symbol": "FB"
-	# 	}
-	# ]
+	array
+
   end
 end
