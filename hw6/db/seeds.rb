1c96d852b01c0e1661b22066b013d76a6fbec0a1
Kristine Zhang <kristinez@berkeley.edu>
2022-03-31 03:06:19 -0700

    clean up seed

--- hw6/db/seeds.rb
+++ hw6/db/seeds.rb
@@ -19,30 +19,12 @@ account1 = Account.create!(gender: "male", age: 30, first_name: "LeBron", last_n
 todolist1 = TodoList.create!(list_name: "list1", list_due_date: Date.new(2001,1,25), account_id: account1.id)
 todolist2 = TodoList.create!(list_name: "list2", list_due_date: Date.new(2001,2,25), account_id: account1.id)
 
-# todolist1 = TodoList.create!(list_name: "list1", list_due_date: Date.new(2001,1,25))
-# todolist2 = TodoList.create!(list_name: "list2", list_due_date: Date.new(2001,2,25))
-
 todoitem1 = TodoItem.create!(due_date: Date.new(2001,1,10), task_title: "Buy tickets", description: "Buy tickets description", todo_list_id: todolist1.id)
 todoitem2 = TodoItem.create!(due_date: Date.new(2001,1,11), task_title: "Reserve hotel", description: "Reserve hotel", todo_list_id: todolist1.id)
 todoitem3 = TodoItem.create!(due_date: Date.new(2001,1,11), task_title: "Bring the car to mechanic", description: "Bring the car to mechanic", todo_list_id: todolist1.id)
 
-# todoitem1 = TodoItem.create!(due_date: Date.new(2001,1,10), task_title: "Buy tickets", description: "Buy tickets description")
-# todoitem2 = TodoItem.create!(due_date: Date.new(2001,1,11), task_title: "Reserve hotel", description: "Reserve hotel")
-# todoitem3 = TodoItem.create!(due_date: Date.new(2001,1,11), task_title: "Bring the car to mechanic", description: "Bring the car to mechanic")
 tag1 = Tag.create!(tag_name: "vacations")
 tag2 = Tag.create!(tag_name: "business trips")
 
 todoitem1.tags << tag1
 todoitem1.tags << tag2
-
-# todoitem2.tags << tag1
-# todoitem2.tags << tag2
-
-# todolist1.todo_items << todoitem1
-# todolist1.todo_items << todoitem2
-# todolist1.todo_items << todoitem3
-
-# account1.todo_lists << todolist1
-# account1.todo_lists << todolist2
-
-# user1.account = account1
