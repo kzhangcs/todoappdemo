be523e941951730d1b2067507f673e5ada7cd06a
Kristine Zhang <kristinez@berkeley.edu>
2022-03-31 21:45:10 -0700

    validate gender and age

--- hw6/app/models/account.rb
+++ hw6/app/models/account.rb
@@ -1,4 +1,7 @@
 class Account < ApplicationRecord
   belongs_to :user
   has_many :todo_lists
+
+  validates :gender, inclusion: { in: ["male","female", "N/A"] }
+  validates :age, numericality: { greater_than_or_equal_to: 20, less_than_or_equal_to: 100 }
 end
