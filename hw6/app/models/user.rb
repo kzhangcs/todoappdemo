4fc8b1108575df568406b84a7cfcb054362ce054
Kristine Zhang <kristinez@berkeley.edu>
2022-03-31 19:47:19 -0700

    map :through todo_items to User

--- hw6/app/models/user.rb
+++ hw6/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
    has_one :account
+   has_many :all_lists, through: :account, source: :todo_lists
+   has_many :all_items, through: :all_lists, source: :todo_items
 end
