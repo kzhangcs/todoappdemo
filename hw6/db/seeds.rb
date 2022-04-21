9a65323c2f8f0b9dd7290d3035b62a8cb1e66138
Kristine Zhang <kristinez@berkeley.edu>
2022-04-20 18:04:13 -0700

    Update seeds.rb with 'done' field in todo_items

--- hw6/db/seeds.rb
+++ hw6/db/seeds.rb
@@ -20,12 +20,12 @@ account2 = Account.create!(gender: "male", age: 30, first_name: "John", last_nam
 todolist1 = TodoList.create!(list_name: "vacation to Disney", list_due_date: Date.new(2022,9,20), account_id: account1.id)
 todolist2 = TodoList.create!(list_name: "business trip to Alaska", list_due_date: Date.new(2022,6,22), account_id: account1.id)
 
-todoitem1 = TodoItem.create!(due_date: Date.new(2022,8,15), task_title: "Buy tickets", description: "Buy tickets description", todo_list_id: todolist1.id)
-todoitem2 = TodoItem.create!(due_date: Date.new(2022,6,20), task_title: "Reserve hotel", description: "Reserve hotel description", todo_list_id: todolist1.id)
-todoitem3 = TodoItem.create!(due_date: Date.new(2022,6,7), task_title: "Bring the car to mechanic", description: "Bring the car to mechanic description", todo_list_id: todolist1.id)
+todoitem1 = TodoItem.create!(due_date: Date.new(2022,8,15), task_title: "Buy tickets", description: "Buy tickets description", todo_list_id: todolist1.id, done: true)
+todoitem2 = TodoItem.create!(due_date: Date.new(2022,6,20), task_title: "Reserve hotel", description: "Reserve hotel description", todo_list_id: todolist1.id, done: false)
+todoitem3 = TodoItem.create!(due_date: Date.new(2022,6,7), task_title: "Bring the car to mechanic", description: "Bring the car to mechanic description", todo_list_id: todolist1.id, done: false)
 
-todoitem4 = TodoItem.create!(due_date: Date.new(2022,4,29), task_title: "Buy a hunting rifle", description: "Buy a hunting rifle description", todo_list_id: todolist2.id)
-todoitem5 = TodoItem.create!(due_date: Date.new(2022,4,28), task_title: "Buy a really warm coat", description: "Buy a really warm coat description", todo_list_id: todolist2.id)
+todoitem4 = TodoItem.create!(due_date: Date.new(2022,4,29), task_title: "Buy a hunting rifle", description: "Buy a hunting rifle description", todo_list_id: todolist2.id, done: true)
+todoitem5 = TodoItem.create!(due_date: Date.new(2022,4,28), task_title: "Buy a really warm coat", description: "Buy a really warm coat description", todo_list_id: todolist2.id, done: false)
 
 tag1 = Tag.create!(tag_name: "vacations")
 tag2 = Tag.create!(tag_name: "trips")
