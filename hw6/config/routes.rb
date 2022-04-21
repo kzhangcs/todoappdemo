239c3fb15bdb5f409afdd5eaf59c210507c5dd30
Kristine Zhang <kristinez@berkeley.edu>
2022-04-20 18:35:13 -0700

    Add todo_items to routes

--- hw6/config/routes.rb
+++ hw6/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "todo_lists#index"
-  resources :todo_lists
+  resources :todo_lists do
+    resources :todo_items
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
