090f00c20722d0f051364e4f1eaa463bf142a125
Kristine Zhang <kristinez@berkeley.edu>
2022-04-20 18:52:49 -0700

    todo_items: show (controller and view)

--- hw6/app/controllers/todo_items_controller.rb
+++ hw6/app/controllers/todo_items_controller.rb
@@ -1,2 +1,17 @@
 class TodoItemsController < ApplicationController
+  before_action :set_todo_item, only: %i[ show edit update destroy ]
+
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_todo_item
+      @todo_item = TodoItem.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def todo_item_params
+      params.require(:todo_item).permit(:task_title, :item_due_date, :description, :done)
+    end
 end
