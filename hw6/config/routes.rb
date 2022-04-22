354282aa89fa68d9c762c7489cc1ff1c05e206d8
Kristine Zhang <kristinez@berkeley.edu>
2022-04-21 17:26:05 -0700

    rails g controller sessions new create destroy add routes

--- hw6/config/routes.rb
+++ hw6/config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'sessions/destroy'
   root "todo_lists#index"
   resources :todo_lists do
     resources :todo_items
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get '/login' => "sessions#new", as: "login"
+  get '/logout' => "sessions#destroy", as: "logout"
 end
