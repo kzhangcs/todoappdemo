9c3c73d39f4921aace220a3196ff395f75a38092
Kristine Zhang <kristinez@berkeley.edu>
2022-04-21 18:57:34 -0700

    add accounts to menu bar, add accounts show, edit to controller/view

--- hw6/app/controllers/accounts_controller.rb
+++ hw6/app/controllers/accounts_controller.rb
@@ -1,10 +1,23 @@
 class AccountsController < ApplicationController
-  def show
-  end
+	before_action :set_account, only: %i[ show edit update ]
 
-  def edit
-  end
+	def show
+	end
 
-  def update
-  end
+	def edit
+	end
+
+	def update
+	end
+
+	private
+	# Use callbacks to share common setup or constraints between actions.
+	def set_account
+	  @account = current_user.account
+	end
+
+	# Only allow a list of trusted parameters through.
+	# def todo_list_params
+	#   params.require(:todo_list).permit(:list_name, :list_due_date)
+	# end
 end
