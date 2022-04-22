b6f304d1fa5e95e8986e913aedd4c2659ee80cbb
Kristine Zhang <kristinez@berkeley.edu>
2022-04-21 17:37:08 -0700

    ensure login for all pages except for login page

--- hw6/app/controllers/application_controller.rb
+++ hw6/app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+	before_action :ensure_login
+
+	protected
+	    def ensure_login
+	      redirect_to login_path unless session[:user_id]
+	    end
 end
