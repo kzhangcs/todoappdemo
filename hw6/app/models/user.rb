814d06b91b2ed630853e9d83e21a415194d03955
Kristine Zhang <kristinez@berkeley.edu>
2022-04-21 17:17:31 -0700

    add has_secure_password

--- hw6/app/models/user.rb
+++ hw6/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+   has_secure_password
+
    has_one :account
    has_many :all_lists, through: :account, source: :todo_lists
    has_many :all_items, through: :all_lists, source: :todo_items
