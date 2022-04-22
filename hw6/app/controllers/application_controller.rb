fa1c9f795902a75e1c83b5af4ddecc9db1b3d485
Kristine Zhang <kristinez@berkeley.edu>
2022-04-21 18:38:12 -0700

    cache

--- hw6/app/controllers/application_controller.rb
+++ hw6/app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
 	before_action :ensure_login
+	before_action :set_cache_buster
+
+	private
+	def set_cache_buster
+	  response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+	  response.headers["Pragma"] = "no-cache"
+	  response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+	end
 
 	protected
 	    def ensure_login
