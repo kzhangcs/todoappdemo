cf6ff1888799406b95de72c42a2b020e1b41a75b
Kristine Zhang <kristinez@berkeley.edu>
2022-05-04 19:00:46 -0700

    fix todo_item_id fail

--- hw6/app/controllers/todo_items_controller.rb
+++ hw6/app/controllers/todo_items_controller.rb
@@ -9,13 +9,12 @@ class TodoItemsController < ApplicationController
   end
 
   def new
-    @todo_item = @todo_list.todo_items.new
+    @todo_item = TodoItem.new
     @todo_item.done = false #todo: initialize todo here?
   end
 
   def create
-    @todo_item = @todo_list.todo_items.new(create_todo_item_params) #todo: limit :done
-
+    @todo_item = current_user.account.todo_lists.todo_items.new(create_todo_item_params) #todo: limit :done
     respond_to do |format|
       if @todo_item.save
         format.html { redirect_to todo_list_todo_item_path(:todo_list_id => @todo_list.id, :todo_item_id => @todo_item.id), notice: 'Todo item was successfully created.' }
@@ -32,6 +31,7 @@ class TodoItemsController < ApplicationController
       if @todo_item.update(todo_item_params)
         format.html { redirect_to todo_list_todo_item_path(:todo_list_id => @todo_list.id, :todo_item_id => @todo_item.id), notice: 'Todo item was successfully updated.' }
         format.json { render :show, status: :ok, location: @todo_item }
+        format.js
       else
         format.html { render :edit }
         format.json { render json: @todo_item.errors, status: :unprocessable_entity }
@@ -64,6 +64,8 @@ class TodoItemsController < ApplicationController
     end
 
     def set_todo_list
-      @todo_list = TodoList.find(params[:todo_list_id]) #todo: change to current_user?
+      @todo_list = current_user.account.todo_lists.find(params[:todo_list_id])
+
+      # @todo_list = TodoList.find(params[:todo_list_id]) #todo: change to current_user?
     end
 end
